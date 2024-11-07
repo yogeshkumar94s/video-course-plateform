a2290dcbf0e9dd19f6490076a295ee0451f144e4
Yogesh Kumar <yksainimbd@gmail.com>
2024-11-08 01:00:06 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["http://localhost:3000"],
+      // bodySizeLimit: "2mb", // default is "1mb"
+    },
+  },
 };
 
 export default nextConfig;
